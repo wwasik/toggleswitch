22a08ed492c19a310a9b114a5af91214c8b85534
wojtek <wojtek@wojtek-efektech>
2013-07-16 15:36:35 +0200

    Extracted css classes to variables

--- public_html/js/toggleswitch.js
+++ public_html/js/toggleswitch.js
@@ -3,17 +3,22 @@ jQuery.fn.extend({
         this.addClass("switch");
         this.html('<div class="switch_rail"><div class="switch_lever"></div></div>');
         this.click(function() {
-            var lefty = $('.switch_lever');
+            var leverClass = 'switch_lever';
+            var railClass = 'switch_rail';
+            var onClass = 'on';
+            
+            var toggleswitch = $(this);
+            var lefty = toggleswitch.find('.'+leverClass);
             lefty.animate({
                 left: (parseInt(lefty.css('left'), 10) === 0) ? lefty.outerWidth() : 0
             },
             function() {
-                var rail = $('.switch_rail');
-                if (rail.hasClass('on')) {
-                    rail.removeClass('on');
+                var rail = toggleswitch.find('.'+railClass);
+                if (rail.hasClass(onClass)) {
+                    rail.removeClass(onClass);
                     offCallback === undefined ? null : offCallback();
                 } else {
-                    rail.addClass('on');
+                    rail.addClass(onClass);
                     onCallback === undefined ? null : onCallback();
                 }
             });

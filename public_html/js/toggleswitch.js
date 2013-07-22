f8e7d455987fa76717578e40970ff57c3dbe1232
wojtek <wojtek@wojtek-efektech>
2013-07-22 10:27:56 +0200

    Made class ToggleSwitch and added reset method

--- public_html/js/toggleswitch.js
+++ public_html/js/toggleswitch.js
@@ -1,27 +1,41 @@
+function ToggleSwitch(container, onCallback, offCallback) {
+    var _SWITCH_CLASS = 'switch';
+    var _LEVER_CLASS = 'switch_lever';
+    var _RAIL_CLASS = 'switch_rail';
+    var _ON_CLASS = 'on';
+    var _toggleSwitch = $(container);
+
+    _toggleSwitch.addClass("switch");
+    _toggleSwitch.html('<div class="switch_rail"><div class="switch_lever"></div></div>');
+    _toggleSwitch.click(function() {
+        var lefty = _toggleSwitch.find('.' + _LEVER_CLASS);
+        lefty.animate({
+            left: (parseInt(lefty.css('left'), 10) === 0) ? lefty.outerWidth() : 0
+        },
+        function() {
+            var rail = _toggleSwitch.find('.' + _RAIL_CLASS);
+            if (rail.hasClass(_ON_CLASS)) {
+                rail.removeClass(_ON_CLASS);
+                offCallback === undefined ? null : offCallback();
+            } else {
+                rail.addClass(_ON_CLASS);
+                onCallback === undefined ? null : onCallback();
+            }
+        });
+    });
+
+    this.reset = function() {
+        if ($(_toggleSwitch).hasClass(_SWITCH_CLASS)) {
+            var rail = _toggleSwitch.find('.' + _RAIL_CLASS);
+            rail.removeClass(_ON_CLASS);
+            var lefty = _toggleSwitch.find('.' + _LEVER_CLASS);
+            lefty.css('left', 0);
+        }
+    };
+}
+
 jQuery.fn.extend({
     toggleswitch: function(onCallback, offCallback) {
-        this.addClass("switch");
-        this.html('<div class="switch_rail"><div class="switch_lever"></div></div>');
-        this.click(function() {
-            var leverClass = 'switch_lever';
-            var railClass = 'switch_rail';
-            var onClass = 'on';
-            
-            var toggleswitch = $(this);
-            var lefty = toggleswitch.find('.'+leverClass);
-            lefty.animate({
-                left: (parseInt(lefty.css('left'), 10) === 0) ? lefty.outerWidth() : 0
-            },
-            function() {
-                var rail = toggleswitch.find('.'+railClass);
-                if (rail.hasClass(onClass)) {
-                    rail.removeClass(onClass);
-                    offCallback === undefined ? null : offCallback();
-                } else {
-                    rail.addClass(onClass);
-                    onCallback === undefined ? null : onCallback();
-                }
-            });
-        });
+        return new ToggleSwitch($(this), onCallback, offCallback);
     }
 });
